076a2e203272959e78dd7f03874b9c41fd40eb40
AlirezaGharib <alirezagharib110@gmail.com>
2024-03-06 23:14:41 +0330

    add send_json function in utilities.rs

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,5 +1,19 @@
+use async_std::prelude::*;
+use serde::Serialize;
 use std::error::Error;
+use std::marker::Unpin;
 
 pub type ChatError = Box<dyn Error + Send + Sync + 'static>;
 
 pub type ChatResult<T> = Result<T, ChatError>;
+
+pub async fn send_json<O, P>(leave: &mut O, data: &P) -> ChatResult<()>
+where
+    O: async_std::io::Write + Unpin,
+    P: Serialize,
+{
+    let mut json:String = serde_json::to_string(&data)?;
+    json.push('\n');
+    leave.write_all(json.as_bytes()).await?;
+    Ok(())
+}
